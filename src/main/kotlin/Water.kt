75e374434dc1a7c193815e1bf72be1bb7d45e707
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-03-14 19:30:08 +0300

    Implement something like a scratch, just a water-line (understand it like as each circle is molecule)

--- src/main/kotlin/Water.kt
+++ src/main/kotlin/Water.kt
@@ -1,2 +1,28 @@
-class Water {
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+
+val color = Color(0xFF14d5c1)
+
+@Composable
+fun Water(modifier: Modifier = Modifier) {
+    Canvas(modifier = modifier) {
+        val points = buildList {
+            for (i in -50..50) {
+                add(Offset(
+                    x = center.x + i * 100f,
+                    y = center.y
+                ))
+            }
+        }
+        points.forEach {
+            drawCircle(
+                color = color,
+                radius = 10f,
+                center = it
+            )
+        }
+    }
 }
